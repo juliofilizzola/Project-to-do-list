40933e28aefc36a9b762d9f9ecabd68ec7bfd829
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-29 00:12:05 -0300

    [second-day] creat setCheck

--- todolist/src/features/todoSlice.js
+++ todolist/src/features/todoSlice.js
@@ -10,10 +10,15 @@ const todoSlice = createSlice({
   reducers: {
     saveTodo:(state, action) => {
       state.todoList.push(action.payload);
+    },
+    setCheck: (state, action) => {
+      state.todoList.map(({id, done}) => {
+       if (action.payload === id) done === true? done = false : done = true
+      })
     }
   }
 });
 
-export const { saveTodo } = todoSlice.actions;
+export const { saveTodo, setCheck } = todoSlice.actions;
 export const selectedTodoList = (state) => state.todosLists.todoList;
 export default todoSlice.reducer;
