99a6d55ea0c3dbbc9384161d315231ae36649595
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-28 23:05:04 -0300

    [second-day] const todoList com map

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -2,9 +2,22 @@ import React from 'react';
 import './App.css';
 import Input from './components/Input'
 
+const todoList = [{
+    item: 'Todo',
+    done: false,
+    id: 57542874,
+},{
+  item: 'Todo2',
+  done: true,
+  id: 235742887
+}]
+
 function App() {
   return (
-    <div className="App">
+    <div className="App">,
+      <div>
+        {todoList.map((itemList) => ())}
+      </div>
       <Input />
     </div>
   );
