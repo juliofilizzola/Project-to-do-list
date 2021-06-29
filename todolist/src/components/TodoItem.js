181ec799cbdfce5f18997e6ead2e455619ee3b06
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-28 23:24:50 -0300

    [second-day] implementando checkbox no componente todoitem

--- todolist/src/components/TodoItem.js
+++ todolist/src/components/TodoItem.js
@@ -1,8 +1,19 @@
 import React from "react";
+import Checkbox from '@material-ui/core/Checkbox';
+
 
 export const TodoItem = ({ name, done, id }) => {
+  const handleChange = () => {
+
+  }
   return (
     <div>
+      <Checkbox
+        checked={ done }
+        onChange={handleChange}
+        name="checkedB"
+        color="primary"
+      />
       <p> {name} </p>
     </div>
   );
