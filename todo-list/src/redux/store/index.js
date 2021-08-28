00496b84569ebbc5289f346bf0049e8383372e61
Filizzola <juliofilizzola@hotmail.com>
2021-08-28 19:31:23 -0300

    edite redux dev tools

--- todo-list/src/redux/store/index.js
+++ todo-list/src/redux/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
-import rootReducer from '../reducers/index';
+import rootReducer from '../reduce/index';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, window.REDUX_DEVTOOLS_EXTENSION && window.REDUX_DEVTOOLS_EXTENSION());
 
 export default store;
