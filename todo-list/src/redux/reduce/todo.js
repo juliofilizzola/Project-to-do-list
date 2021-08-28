4f93e9f4e54f9bb068fb6116bd6ced0f3a3a9476
Filizzola <juliofilizzola@hotmail.com>
2021-08-28 20:08:51 -0300

    configurando reduce todo

--- todo-list/src/redux/reduce/todo.js
+++ todo-list/src/redux/reduce/todo.js
@@ -1,3 +1,19 @@
-export default function todo() {
-  return 0
+import { NEW_TASK } from "../action";
+
+const INITIAL_STATE = {
+  tasks: [],
+};
+
+function todo(state = INITIAL_STATE, action) {
+  switch(action.type) {
+    case NEW_TASK: 
+    return {
+      ...state,
+      tasks: [...action.payload],
+    }
+    default:
+      return state;
+  }
 }
+
+export default todo;
