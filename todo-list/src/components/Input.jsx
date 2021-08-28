487daa3e76d390feff13779eb7db0d614abcbabd
Filizzola <juliofilizzola@hotmail.com>
2021-08-28 19:29:57 -0300

    configurando o store

--- todo-list/src/components/Input.jsx
+++ todo-list/src/components/Input.jsx
@@ -1,9 +1,13 @@
+import React from "react";
+
 function Input() {
+  const [task, setTask] =  React.useState('');
   return (
     <div>
       <label htmlFor="inputTask">
-        <input type="text" name="inputTask"/>
+        <input type="text" name="inputTask" onChange={({ target }) => setTask(target.value)} />
       </label>
+      <div>{task}</div>
     </div>
   )
 }
