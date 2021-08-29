1295d2fa3d80c50ba1c7f1b11f77c2d288dbe919
Filizzola <juliofilizzola@hotmail.com>
2021-08-28 23:04:14 -0300

    modificando função addNewTask

--- todo-list/src/components/Input.jsx
+++ todo-list/src/components/Input.jsx
@@ -4,26 +4,30 @@ import { setNewTask } from '../redux/action/index';
 
 function Input(NewTask) {
   const [task, setTask] =  React.useState('');
+
   const addNewTask = () => {
-    NewTask.NewTask(task)
-  }
+    if(task) {
+      NewTask.NewTask(task);
+      setTask('');
+    }
+  };
+
   return (
     <div>
       <label htmlFor="inputTask">
-        <input type="text" name="inputTask" onChange={({ target }) => setTask(target.value)} />
+        <input type="text" value={task} name="inputTask" onChange={({ target }) => setTask(target.value)} />
       </label>
       <button
         onClick={addNewTask}
       >
         ola
       </button>
-      <div>{task}</div>
     </div>
-  )
+  );
 }
 
 const mapDispatchToProps = (dispatch) => ({
   NewTask: (task) => dispatch(setNewTask(task)),
-})
+});
 
-export default connect(null, mapDispatchToProps)(Input)
+export default connect(null, mapDispatchToProps)(Input);
