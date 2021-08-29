b9170ea41da568a15fdfadd7ed773adfb3d816ac
Filizzola <juliofilizzola@hotmail.com>
2021-08-28 23:00:24 -0300

    renderizando as task

--- todo-list/src/components/Tasks.jsx
+++ todo-list/src/components/Tasks.jsx
@@ -1,11 +1,20 @@
 import { connect } from 'react-redux';
 
-function Tasks() {
+function Tasks(tasks) {
+
   return (
     <div>
-      
+      {tasks.tasks && tasks.tasks.map((task, index ) =>( 
+        <div key={index}>
+          <p>{task}</p>
+        </div>
+      ))}
     </div>
   )
 }
 
-export default connect()(Tasks);
+const mapStateToProps = (state) => ({
+  tasks: state.todo.tasks,
+})
+
+export default connect(mapStateToProps)(Tasks);
--- todo-list/src/pages/Todo.jsx
+++ todo-list/src/pages/Todo.jsx
@@ -1,9 +1,11 @@
 import Input from "../components/Input";
+import Tasks from "../components/Tasks";
 
 function Todo() {
   return (
     <div>
       <Input/>
+      <Tasks/>
     </div>
   );
 }
